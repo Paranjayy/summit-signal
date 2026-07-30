e697efc677a02bb48a35140736f9debc60d35951
Paranjay <kparanjay245@gmail.com>
2026-07-31 05:17:51 +0530

    fix: mount game application root

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
+import { createRoot } from 'react-dom/client'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Environment, Float, Html, Sparkles, Stars } from '@react-three/drei'
 import * as THREE from 'three'
@@ -117,3 +118,5 @@ function App() {
 }
 
 export default App
+
+createRoot(document.getElementById('root')!).render(<App />)
